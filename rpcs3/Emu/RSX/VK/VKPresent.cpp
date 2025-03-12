cfc84b0c25989ebdaf1b966e3cd4fc97e899716e
DH <dh.rpcs3@gmail.com>
2025-03-12 05:07:22 +0300

    test

--- rpcs3/Emu/RSX/VK/VKPresent.cpp
+++ rpcs3/Emu/RSX/VK/VKPresent.cpp
@@ -116,9 +116,8 @@ void VKGSRender::present(vk::frame_context_t *ctx)
 		case VK_SUCCESS:
 			break;
 		case VK_SUBOPTIMAL_KHR:
-#ifndef ANDROID
+			rsx_log.warning("Swapchain present returns SUBOPTIMAL");
 			should_reinitialize_swapchain = true;
-#endif
 			break;
 		case VK_ERROR_OUT_OF_DATE_KHR:
 			swapchain_unavailable = true;
